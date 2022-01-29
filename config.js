b946ad92f08be060b76eeac7b4af6c5df3508d23
anhhhhhoisd <98635903+anhhhhhoisd@users.noreply.github.com>
2022-01-29 16:06:25 +0700

    Update config.js
    gh-pages

--- config.js
+++ config.js
@@ -1,19 +1,19 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
+    introTitle: 'Trúc à!',
+    introDesc: `Giờ này mặt trời của em đã là ai
+    Vì giờ này trong trái tim anh là em
+    Và rồi người có thấy ko You are tired
+    Sao cứ quần quanh mãi trong tâm trí anh`,
     btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
+    title: 'Xin em đừng rủ nỗi nhớ đi xa 😙',
+    desc: 'Vì màn đêm xuống bầu trời toàn kí ức hai ta',
     btnYes: 'Vẫn cứ là thích anh <33',
     btnNo: 'Không, Anh trai à :3',
     question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
     btnReply: 'Gửi cho anh <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
+    messDesc: 'yêu nhau nha',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
